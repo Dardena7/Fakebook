578e4c3643100ae2d056087388100cd03bc98a5f
Alexis <alexis.dardenne7@gmail.com>
2017-06-30 11:38:30 +0200

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+	test "" do
+		get signup_path
+	end
+	
+	test "invalid signup information" do
+		get signup_path
+		assert_select "form[action=?]", "/signup"
+		assert_no_difference "User.count" do
+			post users_path, params: { user: { name: "",
+																				 email: "user@invalid",
+																				 password: "foo",
+																				 password_confirmation: "bar"
+																 } 
+											 }
+		end
+		assert_template 'users/new'
+		assert_select "form[action=?]", "/signup"
+		assert_select 'div#error_explanation'
+		assert_select 'div.field_with_errors'
+	end
+	
+	test "valid signup information" do
+		get signup_path
+		assert_difference "User.count" do
+			post users_path, params: { user: { name: "example",
+																				 email: "emailexample8z7d5z@validemail.com",
+																				 password: "Hello1234",
+																				 password_confirmation: "Hello1234"
+																}
+											}
+		end
+		follow_redirect!
+		assert_template 'users/show'
+		assert_not flash.empty?
+	end
+
+end
